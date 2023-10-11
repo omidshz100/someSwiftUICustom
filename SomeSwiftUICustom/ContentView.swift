7f76fb26e94df5f8abc1ec7b1797712935911b67
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-10-11 14:38:28 +0200

    Initial Commit

---
+++ SomeSwiftUICustom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SomeSwiftUICustom
+//
+//  Created by Omid Shojaeian Zanjani on 11/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SomeSwiftUICustom/SomeSwiftUICustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeSwiftUICustomApp.swift
+//  SomeSwiftUICustom
+//
+//  Created by Omid Shojaeian Zanjani on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SomeSwiftUICustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
